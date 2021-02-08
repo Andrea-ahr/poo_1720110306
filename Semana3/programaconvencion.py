843e2d2a5149d211ec5fdc739a8c00440d96a448
ANDREA HERN�NDEZ <1720110306@utectulancingo.edu.mx>
2021-02-08 04:16:14 +0000

    programaconvencion.py en funcion

---
+++ Semana3/programaconvencion.py
@@ -0,0 +1,11 @@
+import math
+
+print("Ingresa los valores para la coordenada 1:")
+x1 = int(input("Ingrese el valor de y1:"))
+y1 = int(input("Ingrese el valor de x2:"))
+print("Ingresa los valores para la coordenada 2:")
+x2 = int(input("Ingrese el valor de x2:"))
+y2 = int(input("Ingrese el valor de x2:"))
+
+distancia = math.sqrt((x2-x1)*2+(y2-y1)*2)
+print("La distancia es:", + distancia) 
