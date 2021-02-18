2409ed5660828fb56c45db4af56945da0d39a8ba
ANDREA HERN�NDEZ <1720110306@utectulancingo.edu.mx>
2021-02-18 03:07:38 +0000

    programacalculos.py en funcion

---
+++ Semana4/programacalculos.py
@@ -0,0 +1,30 @@
+'''
+Andrea Hernandez Riveros
+TIC 21
+14 - feb - 2021
+Pedir numero de calculos que se desea calcular y posteriormete realizar el calculo de los segundos
+'''
+
+class LeerDatos():
+
+    dias = 0
+
+    def __init__(self):
+        pass
+
+    def LeerDias(self):
+        self.dias = input("Escribe los dias")
+
+objeto = LeerDatos()
+print(type(objeto.dias))
+print(type(objeto.dias))
+print(type(objeto))
+
+segundo = 1
+minuto = 60
+hora = 3600
+dia = 86400
+print("Dame los dias")
+dias = int(input(""))
+calculos = dia * dias
+print("Numero de segundos es:", + calculos)
